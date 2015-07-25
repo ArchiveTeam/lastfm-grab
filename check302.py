4dd8cec6fcde23d0ac06121311be761398c638fc
Arkiver2 <Arkiver@hotmail.com>
2015-07-25 19:42:48 +0200

    check302.py: check if file exists, handle if
    redirect to same url, handle multiple redirects

--- check302.py
+++ check302.py
@@ -1,9 +1,12 @@
 import sys
 import requests
+import os
 
 def main():
     url = sys.argv[1]
     tries = 0
+    if os.path.isfile('302file'):
+        os.remove('302file')
     while tries < 6:
         print("Fetching {1}, try {0}".format(tries + 1, url))
         sys.stdout.flush()
@@ -12,7 +15,23 @@ def main():
             print("Succes!")
             sys.stdout.flush()
             redirfile = open('302file', 'w')
+            for redirurl in html.history:
+                redirfile.write(redirurl.url + "\n")
+                print(redirurl.url)
+                sys.stdout.flush()
             redirfile.write(html.url)
+            print(html.url)
+            sys.stdout.flush()
+            redirfile.close()
+            break
+        elif html.status_code == 404:
+            print("Succes!")
+            sys.stdout.flush()
+            redirfile = open('302file', 'w')
+            redirfile.write(html.url + "\n")
+            redirfile.write(html.url)
+            print(html.url)
+            sys.stdout.flush()
             redirfile.close()
             break
         print("Received status code {0}, retrying to fetch url".format(html.status_code))
@@ -22,7 +41,6 @@ def main():
         print("Exception fetching {0}".format(url))
         sys.stdout.flush()
         raise Exception
-        
 
 if __name__ == '__main__':
     main()
