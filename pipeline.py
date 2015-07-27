121a9ffe4cd07d6052aa8b0b6e404c7af7ccb410
Arkiver2 <Arkiver@hotmail.com>
2015-07-27 20:24:54 +0200

    pipeline.py: bump version

--- pipeline.py
+++ pipeline.py
@@ -57,7 +57,7 @@ if not WGET_LUA:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = "20150726.03"
+VERSION = "20150727.01"
 USER_AGENT = 'ArchiveTeam'
 TRACKER_ID = 'lastfm'
 TRACKER_HOST = 'tracker.archiveteam.org'
